610238d429cccb8d1c0aaccc2709cd913d7f96de
Yus314 <shizhaoyoujie@gmail.com>
2025-08-08 12:23:19 +0900

    feat: add fcitx5-cskk package - SKK input method for Fcitx5
    Add fcitx5-cskk package for SKK input in Fcitx5:
    - Version: 1.2.0
    - CMake-based build with Qt support options
    - Integrates with cskk library and SKK dictionaries
    - Configurable Qt5/Qt6 support via enableQt parameter
    
    Dependencies:
    - fcitx5 core framework
    - cskk library for SKK functionality
    - SKK dictionaries for Japanese input
    - Optional Qt integration for GUI components
    
    Note: Currently requires nixpkgs fcitx5 >= 5.1.13

---
+++ pkgs/fcitx5-cskk/default.nix
@@ -0,0 +1,59 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  cmake,
+  extra-cmake-modules,
+  pkg-config,
+  gettext,
+  fcitx5,
+  fcitx5-qt,
+  qtbase,
+  wrapQtAppsHook,
+  cskk,
+  skkDictionaries,
+  enableQt ? false,
+  useQt6 ? false,
+}:
+stdenv.mkDerivation rec {
+  pname = "fcitx5-cskk";
+  version = "1.2.0";
+  
+  src = fetchFromGitHub {
+    owner = "fcitx";
+    repo = "fcitx5-cskk";
+    rev = "6d4407c64df46423c378afeefa71bda3282a7cec";
+    hash = "sha256-UfEmRenWiX2xbIirkDRbix1YQrDz1/sVg6yut8ZRJ0k=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    extra-cmake-modules
+    pkg-config
+    gettext
+  ]
+  ++ lib.optional enableQt wrapQtAppsHook;
+
+  buildInputs = [
+    fcitx5
+    cskk
+  ]
+  ++ lib.optionals enableQt [
+    fcitx5-qt
+    qtbase
+  ];
+
+  cmakeFlags = [
+    "-DENABLE_QT=${toString enableQt}"
+    "-DUSE_QT6=${toString useQt6}"
+    "-DSKK_DICT_DEFAULT_PATH=${skkDictionaries.l}/share/skk/SKK-JISYO.L"
+  ];
+
+  meta = with lib; {
+    description = "SKK input method engine for Fcitx5 using libcskk";
+    homepage = "https://github.com/fcitx/fcitx5-cskk";
+    license = licenses.gpl3Plus;
+    maintainers = [ ];
+    platforms = platforms.linux;
+  };
+}
