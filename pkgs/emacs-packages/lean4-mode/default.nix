ce4619b0f56a81271d878add8bb2e7c06e9db069
Yus314 <shizhaoyoujie@gmail.com>
2025-12-13 21:54:26 +0900

    lean4-mode: add files attribute to include data directory

--- pkgs/emacs-packages/lean4-mode/default.nix
+++ pkgs/emacs-packages/lean4-mode/default.nix
@@ -20,6 +20,10 @@ melpaBuild rec {
     hash = "sha256-DLgdxd0m3SmJ9heJ/pe5k8bZCfvWdaKAF0BDYEkwlMQ=";
   };
 
+  files = [
+    "*.el"
+    "data"
+  ];
   meta = {
     description = "Emacs major mode for Lean 4";
     homepage = "https://github.com/leanprover-community/lean4-mode";
